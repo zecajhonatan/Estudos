0b31a3e245beb21217841a1365344aec2fd432bc
zecaJhonatan <zecajhonatan@gmail.com>
2024-01-05 17:13:38 -0300

    Atualização no projeto appLemos

--- appLemos/app.js
+++ appLemos/app.js
@@ -1,14 +1,24 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 let app = express()
 const PORT = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // diz ao express que o motor de renderização do html vai ser o ejs
 app.set('view engine', 'ejs')
 // diz ao express que vai receber dados em formato de 'json'
 app.use(express.json())
 
+// ROTA PRINCIPAL
 app.get('/', (req, res) => {
   res.render('cadastro')
+})
+
+app.post('/buscarDados', (req, _) => {
+  let dados = req.body
+  console.log(dados)
 })
 
 
