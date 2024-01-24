8feac9e6c9f8073b811c157a204cc6305e5acc4c
zecaJhonatan <zecajhonatan@gmail.com>
2024-01-24 17:37:44 -0300

    finalizei o estudo de api e adicionei um projeto de chat com socket.io

--- programacaoAssincrona/index.js
+++ programacaoAssincrona/index.js
@@ -1,35 +1,62 @@
+// programação (sincrona) --> onde cada instrução, função desejada vai ser executada por vez 
+
+
+// CODIGO ASSINCRONO SENDO CONSTRUIDO COM CALLBACKS
+
 // let de = 'Zecajhonatan'
 // let para = 'zecajhonatan@gmail.com'
-// let corpo = 'dados enviados por email'
+// let corpo = 'dados enviados para o email'
 
-// function enviarEmail(callbeck) {
+// function enviarEmail(callback) {
 //   setTimeout(() => {
-//     let deuErro = true
+//     let deuErro = false
 //     if (deuErro) {
-//       callbeck('O envio de email Falhou!')
+//       callback("o envio do E-mail falhou!")
 //     } else {
-//       callbeck()
+//       callback()
 //     }
-//   }, 8000)
+//   }, 3000)
 // }
 
-// console.log('inicio do envio') // sincrono 
-// enviarEmail((error) => {
+// function callback(error) {
 //   if (error == undefined) {
-//     console.log('Não deu erro')
+//     console.log("Tudo OK")
 //   } else {
-//     console.log(error)
+//     console.log("Deu Erro: " + error)
 //   }
-// }) // assincrono 
+// }
 
+// console.log("Inicio do envio") // SINCRONO
+// enviarEmail(callback) // ASSINCRONO
 
-// function enviarEmail(corpo, para) {
+// ----------------------------------------------------------------
+
+// CODIGO ASSINCRONO SENDO CONSTRUIDO COM PROMISE
+
+// function pegarId() {
 //   return new Promise((resolve, reject) => {
 //     setTimeout(() => {
-//       let error = true
-//       console.log('E-mail enviado')
+//       console.log("Pegou o ID")
+//       resolve(5)
+//     }, 2000)
+//   })
+// }
+
+// function buscarEmailBanco(id) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Pegou o E-mail")
+//       resolve("zecajhonatan@gmail.com")
+//     }, 2000)
+//   })
+// }
+
+// function enviarEmail(email) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       let error = false
 //       if (!error) {
-//         resolve()
+//         resolve("E-mail enviado")
 //       } else {
 //         reject('não consegui fazer o que eu te prometi, Desculpe')
 //       }
@@ -37,28 +64,64 @@
 //   })
 // }
 
-// enviarEmail()
-//   .then(() => {
-//     console.log('Voce Conseguiu fazer o que me prometeu!')
-//   })
-//   .catch(error => console.log(error))
-
-// function pegarDadosBanco() {
-//   return new Promise((resolve, reject) => {
-//     let sql = 'SELECT * FROM table WHERE email = email;'
-//     conexao.query(sql, (error, result) => {
-//       if (error) reject(error)
-//       return resolve(JSON.stringify(result))
+// console.log("Inicio")
+// pegarId().then((id) => {
+//   buscarEmailBanco(id).then((email) => {
+//     enviarEmail(email).then((msg) => {
+//       console.log(msg)
+//       console.log(email)
+//       console.log("E-mail enviado para o cliente com o id: " + id)
+//     }).catch((error) => {
+//       console.log(error);
 //     })
 //   })
+// })
+
+// async function update() {
+//   let id = await pegarId()
+//   let email = await buscarEmailBanco(id)
+//   try {
+//    await enviarEmail(email)
+//     console.log("Email enviado com sucesso!!!")
+//   } catch (error) {
+//     console.log(error);
+//   }
 // }
-// pegarDadosBanco()
-//   .then((result) => {
-//     console.log(result)
+
+// update()
+
+// console.log("Fim")
+
+// ----------------------------------------------------------------
+
+// CODIGO ASSINCRONO SENDO CONSTRUIDO COM ASYNC / AWAIT
+
+// let usuarios = {
+//   usuario1: "Jose",
+//   usuario2: "João",
+//   usuario3: "Jhonatan"
+// }
+
+// function pegarUsuarios() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       let certo = true
+//       if (certo) {
+//         resolve(usuarios)
+//       } else {
+//         reject("Não conseguiu buscar os usuarios!")
+//       }
+//     }, 3000)
 //   })
-//   .catch(error => {
-//     console.log({'error:': error })
-//   })
+// }
+
+
+// async function dados() {
+//   let dado = await pegarUsuarios()
+//   console.log(dado);
+// }
+
+// dados()
 
 
 
