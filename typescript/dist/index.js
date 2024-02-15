54bae41914721f54b0f00f1da2023dba96e03b36
zecaJhonatan <zecajhonatan@gmail.com>
2024-02-15 17:12:21 -0300

    Adicionado pasta com arquivos relacionados com typescript

---
+++ typescript/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let age = 5;
+let firshName = "Jhonatan";
+const isValue = true;
+let idk = 5;
+const numeros = [1, 3, 36, 4569];
+let booleans = [false, true, false];
+let nomes = ['Jhonatan', 'Santos'];
+// tupla
+const tupla = [5, 'Jhonatan', false];
---
+++ typescript/src/index.ts
@@ -0,0 +1,19 @@
+let age: number = 5;
+let firshName: string = "Jhonatan";
+const isValue: boolean = true;
+let idk: any = 5
+
+const numeros: number[] = [1,3,36,4569]
+let booleans: boolean[] = [false, true, false]
+let nomes: string[] = ['Jhonatan', 'Santos']
+
+// tupla
+const person: [number, string, boolean] = [5, 'Jhonatan', false]
+// list tupla
+const people: [number, string][]= [
+  [5, 'Zeca'],
+  [10, 'Jhonatan']
+]
+
+// Intersections
+let productionId: number | string = '1'
